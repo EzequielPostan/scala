50f49c7f344ca199291a5b9574d7dfad819fb65e
Ezequiel Postan <ezequiel_postan@hotmail.com>
2018-04-29 20:04:01 -0300

    Solve Bob problem

--- exercises/bob/src/main/scala/Bob.scala
+++ exercises/bob/src/main/scala/Bob.scala
@@ -1,3 +1,17 @@
 object Bob {
-  def response(statement: String): String = ???
+
+  def response(statement: String): String = {
+    def isAQuestion: Boolean = statement.trim.endsWith("?")
+    def isYelling: Boolean = {
+      val letters: String = statement.filter(_.isLetter)
+      letters.nonEmpty && letters.forall(_.isUpper)
+    }
+    def saysNothing: Boolean = statement.trim.isEmpty
+
+    if(isAQuestion && isYelling) "Calm down, I know what I'm doing!"
+    else if (isAQuestion) "Sure."
+    else if (isYelling) "Whoa, chill out!"
+    else if (saysNothing) "Fine. Be that way!"
+    else "Whatever."
+  }
 }
