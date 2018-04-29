41f02bc85b19a59cddb3aadfe091e1862b0b0a21
Ezequiel Postan <ezequiel_postan@hotmail.com>
2018-04-29 17:30:09 -0300

    Solve AtbashCipher problem

--- exercises/atbash-cipher/example.scala
+++ exercises/atbash-cipher/example.scala
@@ -1,4 +1,4 @@
-object AtbashCipher {
+object AtbashCipherExample {
   private def substitute(c: Char) =
     if (c.isDigit) c.toString
     else if (c.isLetter) ('a' + ('z' - c.toLower)).toChar.toString
