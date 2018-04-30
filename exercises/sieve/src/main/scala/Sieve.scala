7586396d95da0485040ec1ee453f7f18f9ca4c7b
Ezequiel Postan <ezequiel_postan@hotmail.com>
2018-04-30 20:35:13 -0300

    Use Hakell like solution for Sieve problem

--- exercises/sieve/src/main/scala/Sieve.scala
+++ exercises/sieve/src/main/scala/Sieve.scala
@@ -1,12 +1,10 @@
 object Sieve {
-  def primes(upperBound: Int): List[Int] = {
-    def removeMultiples(p: Int, ls: List[Int]): List[Int] = ls filter (_ % p != 0)
 
-    @scala.annotation.tailrec
-    def primesTailRec(primesList: List[Int], candidates: List[Int]): List[Int] = candidates match {
-      case Nil   => primesList.reverse
-      case p::ps => primesTailRec(p :: primesList, removeMultiples(p, candidates))
-    }
-    primesTailRec(Nil, (2 to upperBound).toList)
-  }
+  def sieve(s: Stream[Int]): Stream[Int] =
+    s.head #:: sieve(s.tail filter(_ % s.head != 0))
+
+  private val primes = sieve(Stream.from(2))
+
+  def primes(upperBound: Int): List[Int] =
+    (primes takeWhile (_ <= upperBound)).toList
 }
