16590bb60f921fefae6cd0258d36e8f5c83d37d8
Ezequiel Postan <ezequiel_postan@hotmail.com>
2018-05-01 19:59:47 -0300

    Solve sum-of-multiples problem

--- exercises/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ exercises/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,4 +1,8 @@
 object SumOfMultiples {
-  def sum(factors: Set[Int], limit: Int): Int = ???
+  
+  def sum(factors: Set[Int], limit: Int): Int =
+    (for(f <- factors;
+         i <- f until limit by f
+    ) yield i).sum
 }
 
