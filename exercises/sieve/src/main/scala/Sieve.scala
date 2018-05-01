87c50218caadc0edf8e4c8dfe4acde42e76128e0
Ezequiel Postan <ezequiel_postan@hotmail.com>
2018-05-01 00:59:24 -0300

    Solve nth-prime problem

--- exercises/sieve/src/main/scala/Sieve.scala
+++ exercises/sieve/src/main/scala/Sieve.scala
@@ -1,7 +1,11 @@
 object Sieve {
 
+  // This is an elegant but inneficient solution
+  // A different solution was implemented for comparison in
+  // Nth-prime problem
+
   def sieve(s: Stream[Int]): Stream[Int] =
-    s.head #:: sieve(s.tail filter(_ % s.head != 0))
+    s.head #:: sieve(s.tail filter (_ % s.head != 0))
 
   private val primes = sieve(Stream.from(2))
 
