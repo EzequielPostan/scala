c67c063f774b3df578f6ff9d511faf7c0e72e37e
Ezequiel Postan <ezequiel_postan@hotmail.com>
2018-04-30 21:19:31 -0300

    Solve difference-of-squares problem

--- exercises/difference-of-squares/src/main/scala/DifferenceOfSquares.scala
+++ exercises/difference-of-squares/src/main/scala/DifferenceOfSquares.scala
@@ -1,8 +1,8 @@
 object DifferenceOfSquares {
 
-  def sumOfSquares(n: Int): Int = ???
+  def sumOfSquares(n: Int): Int = (n*(n + 1) *(2 * n + 1)) / 6
 
-  def squareOfSum(n: Int): Int = ???
+  def squareOfSum(n: Int): Int = (n*n*(n+1)*(n+1)) / 4
 
-  def differenceOfSquares(n: Int): Int = ???
+  def differenceOfSquares(n: Int): Int = squareOfSum(n) - sumOfSquares(n)
 }
