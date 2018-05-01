53de72772585255ed4a0df7d3f84cebb16a1ebb7
Ezequiel Postan <ezequiel_postan@hotmail.com>
2018-04-30 21:28:45 -0300

    Solve Etl problem

--- exercises/etl/example.scala
+++ exercises/etl/example.scala
@@ -1,4 +1,4 @@
-object Etl {
+object EtlExample {
   def transform(m: Map[Int, Seq[String]]): Map[String, Int] = m.flatMap { case (key, strings) =>
     Map(strings.map(_.toLowerCase -> key): _*)
   }
