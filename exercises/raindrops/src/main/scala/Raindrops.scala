2ae949926e0f1d31f663bf1a79a32ff6463c92c0
Ezequiel Postan <ezequiel_postan@hotmail.com>
2018-05-01 22:15:16 -0300

    Solve Raindrops problem

--- exercises/raindrops/src/main/scala/Raindrops.scala
+++ exercises/raindrops/src/main/scala/Raindrops.scala
@@ -1,4 +1,16 @@
 object Raindrops {
-  def convert(n: Int): String = ???
+  def convert(n: Int): String = {
+    val pling: String = if (n % 3 == 0) "Pling" else ""
+    val plang: String = if (n % 5 == 0) "Plang" else ""
+    val plong: String = if (n % 7 == 0) "Plong" else ""
+
+    if(pling.isEmpty && plang.isEmpty && plong.isEmpty) n.toString
+    else List(pling, plang, plong).mkString
+  }
+
+
+
+
+
 }
 
